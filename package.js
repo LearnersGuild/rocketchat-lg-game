f3fa9613625579b67edb64b09756cfe7faa32367
jeffreywescott <jeffrey@learnersguild.org>
2016-04-24 12:05:54 -0700

    specify SSO version

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
 
   api.use([
     'ecmascript',
-    'learnersguild:rocketchat-lg-sso',
+    'learnersguild:rocketchat-lg-sso@0.3.0',
   ])
   api.use([
     'rocketchat:lib@0.0.1'
