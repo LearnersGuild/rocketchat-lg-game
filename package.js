8e660ae6c29dd077557f4c010d822e9bbd521a04
jeffreywescott <jeffrey@learnersguild.org>
2016-05-19 09:05:57 -0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'learnersguild:rocketchat-lg-slash-commands',
-  version: '0.4.1',
+  version: '0.4.2',
   summary: 'Custom /slash commands for Rocket.Chat within Learners Guild.',
   git: 'https://github.com/LearnersGuild/rocketchat-lg-slash-commands'
 })
