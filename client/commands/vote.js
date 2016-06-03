6f8a47bdd705bcb7d694c2d847b26ba14062ba89
jeffreywescott <jeffrey@learnersguild.org>
2016-06-03 08:08:57 -0700

    removing extraneous log statement

--- client/commands/vote.js
+++ client/commands/vote.js
@@ -1,7 +1,6 @@
 /* global commandFuncs, window, openFlexPanel */
 
 commandFuncs.vote = args => {
-  console.log({args})
   if (!args.help && (args._.length === 0 || args._.length >= 2)) {
     const gameURL = window.location.href.match(/chat.learnersguild.org/) ? 'https://game.learnersguild.org' : 'http://game.learnersguild.dev'
     const flexPanelURL = `${gameURL}/cycle-voting-results#${Date.now()}`
