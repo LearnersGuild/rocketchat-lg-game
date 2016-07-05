2975108e2b39936ea4a8d0b199e90d5ddfb2b592
jeffreywescott <jeffrey@learnersguild.org>
2016-07-05 15:18:00 -0700

    echo back the command typed-in by the user
    (fixes #39)

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const commands = Npm.require('@learnersguild/game-cli')
 lastSlashCommandRoomIds = {}
 function invoke(command, commandParamStr, commandInfo) {
   console.log(`[LG SLASH COMMANDS] '/${command}' invoked with '${commandParamStr}'`)
+  notifyUser(commandInfo.rid, `> /${command} ${commandParamStr}`)
   const commandFunc = commandFuncs[command].invoke
   if (commandFunc) {
     const {lgJWT, lgPlayer, lgUser} = Meteor.user().services.lgSSO
